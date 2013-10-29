5f878792a6f5a1a8f514437bd9574ed738065aa6
Seth Puckett <puckett.seth@gmail.com>
2013-10-29 13:49:31 -0400

    Updating to utilize ComponentFactory

--- src/com/game/geodetective/GeoDetectiveActivity.java
+++ src/com/game/geodetective/GeoDetectiveActivity.java
@@ -2,28 +2,14 @@ package com.game.geodetective;
 
 import android.os.Bundle;
 
-import com.game.geodetective.graphics.GeoDetectiveSpriteHelper;
-import com.game.geodetective.sound.GeoDetectiveMusicHelper;
-import com.game.geodetective.utility.GeoDetectiveCommonData;
-import com.game.geodetective.utility.GeoDetectiveGameSettings;
+import com.game.geodetective.utility.GeoDetectiveComponentFactory;
 import com.game.loblib.LobLibActivity;
-import com.game.loblib.utility.Global;
 
 public class GeoDetectiveActivity extends LobLibActivity {
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
-        _spriteHelper = new GeoDetectiveSpriteHelper();
-        _musicHelper = new GeoDetectiveMusicHelper();
-        _commonData = new GeoDetectiveCommonData();
-        
-        //TODO: testing
-  		//Editor editor = getSharedPreferences("game", Context.MODE_PRIVATE).edit();
-  		//editor.putInt("maxLevel", 21);
-  		//editor.commit();
-        
+		_componentFactory = new GeoDetectiveComponentFactory();
         super.onCreate(savedInstanceState);
-        
-        Global.Settings = new GeoDetectiveGameSettings();
     } 
 }
---
+++ src/com/game/geodetective/utility/GeoDetectiveComponentFactory.java
@@ -0,0 +1,46 @@
+package com.game.geodetective.utility;
+
+import com.game.geodetective.collision.GeoDetectiveCollisionManager;
+import com.game.geodetective.graphics.GeoDetectiveSpriteHelper;
+import com.game.geodetective.screen.GeoDetectiveScreenManager;
+import com.game.geodetective.sound.GeoDetectiveMusicHelper;
+import com.game.loblib.collision.CollisionManager;
+import com.game.loblib.graphics.SpriteHelper;
+import com.game.loblib.screen.ScreenManager;
+import com.game.loblib.sound.MusicHelper;
+import com.game.loblib.utility.CommonData;
+import com.game.loblib.utility.ComponentFactory;
+import com.game.loblib.utility.GameSettings;
+
+public class GeoDetectiveComponentFactory extends ComponentFactory {
+	
+	@Override
+	public CollisionManager CreateCollisionManager() {
+		return new GeoDetectiveCollisionManager();
+	}
+	
+	@Override
+	public SpriteHelper CreateSpriteHelper() {
+		return new GeoDetectiveSpriteHelper();
+	}
+	
+	@Override
+	public ScreenManager CreateScreenManager() {
+		return new GeoDetectiveScreenManager();
+	}
+	
+	@Override
+	public MusicHelper CreateMusicHelper() {
+		return new GeoDetectiveMusicHelper();
+	}
+	
+	@Override
+	public CommonData CreateCommonData() {
+		return new GeoDetectiveCommonData();
+	}
+	
+	@Override
+	public GameSettings CreateGameSettings() {
+		return new GeoDetectiveGameSettings();
+	}
+}
